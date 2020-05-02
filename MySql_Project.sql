edd730b6686940df573903c81a6c9a48f4e989ab
Jessie Galbraith <galbraithjessie@gmail.com>
2020-05-02 13:14:12 -0600

    Finished multiple queries
    Finished a few queries for project

--- MySql_Project.sql
+++ MySql_Project.sql
@@ -26,15 +26,17 @@ JOIN Courses
 ON Courses.courses_students_id = students_id;
 
 --Summary report of courses and their average grades, sorted by the most challenging course(with the lowest average grade) to the easiest
-SELECT courses_name, AVGgrades_student_grade)
+SELECT courses_name, AVG(grades_student_grade) AS GPA
 FROM Courses
-
+JOIN Grades
+ON Grades.grades_courses_id = courses_id
+GROUP BY courses_name
 ORDER BY GPA DESC;
+
 --Query for finding which student and professor have the most courses in common
-SELECT student_name, professor_name
+SELECT students_name, professors_name
 FROM Courses 
 JOIN Students
 ON Courses.courses_students_id = students_id
 JOIN Professors
-ON Courses.courses_professors_id = professor_id
-GROUP BY courses_id;
+ON Courses.courses_professors_id = professor_id;
